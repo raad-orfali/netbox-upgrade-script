d80601ed4639d48575b4624fa0f9098929ae12b6
raad-orfali <raad_orfali@yahoo.com>
2022-10-18 08:25:23 +0300

    correct /media permission to be able to save images

--- nbupgrade.sh
+++ nbupgrade.sh
@@ -6,6 +6,8 @@
 #                                       #
 # Created on: 28/09/2022                #
 #                                       #
+# Modified on: 18/10/2022               #
+#                                       #
 #########################################
 
 # Download and extract the update version
@@ -18,8 +20,10 @@ cp /opt/netbox/local_requirements.txt /opt/netbox-$1
 cp /opt/netbox/netbox/netbox/configuration.py /opt/netbox-$1/netbox/netbox/
 cp /opt/netbox/netbox/netbox/ldap_config.py /opt/netbox-$1/netbox/netbox/
 
-# copy the media files
+# copy the media files and modify permission
 cp -pR /opt/netbox/netbox/media/ /opt/netbox-$1/netbox/
+cd /opt/netbox/netbox
+chown -R netbox:netbox media/
 
 # copy the gunicorn file
 cp /opt/netbox/gunicorn.py /opt/netbox-$1/
